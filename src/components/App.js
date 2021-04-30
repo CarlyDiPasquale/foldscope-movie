9aac3ecb4c452f699541bccaa605fbfcb098b6ab
Carly DiPasquale <72526507+CarlyDiPasquale@users.noreply.github.com>
2021-04-30 14:21:36 -0400

    Fixed errors causing the app to not load

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer, useEffect } from "react";
 import "../App.css";
 import Header from "./Header";
 import Movie from "./Movie";
@@ -83,9 +83,9 @@ const App = () => {
 
     return (
     <div className="App">
-      <Header text="HOOKED" />
+      <Header text="Movies Galore" />
       <Search search={search} />
-      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <p className="App-intro">Some featured films:</p>
       <div className="movies">
         {loading && !errorMessage ? (
           <span>loading... </span>
